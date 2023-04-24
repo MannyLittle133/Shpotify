6215c76023559e925bdbbd3222b4f8efc8a4a33b
Emmanuel Little <109988822+MannyLittle133@users.noreply.github.com>
2023-04-23 23:35:37 -0400

    models

--- app/models/playlist.rb
+++ app/models/playlist.rb
@@ -17,23 +17,25 @@ class Playlist < ApplicationRecord
         class_name: :User
         
         
-        has_many :playlist_songs,
+    has_many :playlist_songs,
         primary_key: :id,
         foreign_key: :playlist_id,
         class_name: :PlaylistSong,
         dependent: :destroy
         
-        has_many :artists,
-        through: :albums,
-        source: :artist
-        
-        has_many :songs,
-            through: :playlist_songs,
-            source: :song
-            
+    has_many :songs,
+        through: :playlist_songs,
+        source: :song
+
     has_many :albums,
         through: :songs,
         source: :album
+    
+    has_many :artists,
+        through: :albums,
+        source: :artist
+        
+            
 
 
     def self.search(search)
